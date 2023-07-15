ffd46737f5daff86a93321d2fcfdceae7a07fa82
Nicolas Echeverry <nicoecheverry007@gmail.com>
2023-07-15 15:07:22 -0500

    Homework 1: Spaceship
    Diagonal movement is activated or 2 keys are allowed to be pressed at the same time, and it no longer has a movement limit between the side edges, in addition to that, when trying to pass between edges, the image of the ship is seen on both sides

--- game/components/spaceship.py
+++ game/components/spaceship.py
@@ -6,8 +6,10 @@ from game.utils.constants import SCREEN_HEIGHT, SCREEN_WIDTH, SPACESHIP
 class Spaceship(Sprite):
     X_POS = (SCREEN_WIDTH//2)-30
     Y_POS = 500 
+    SPACESHIP_HEIGHT = 60 
+    SPACESHIP_WIDTH = 50
     def __init__(self):
-        self.image = pygame.transform.scale(SPACESHIP,(60,50))
+        self.image = pygame.transform.scale(SPACESHIP,(self.SPACESHIP_HEIGHT, self.SPACESHIP_WIDTH))
         self.rect = self.image.get_rect()
         self.rect.x = self.X_POS
         self.rect.y = self.Y_POS
@@ -15,22 +17,24 @@ class Spaceship(Sprite):
     def update(self, user_input):
         if user_input[pygame.K_LEFT] or user_input[pygame.K_a]:
             self.move_left()
-        elif user_input[pygame.K_RIGHT] or user_input[pygame.K_d]:
+        if user_input[pygame.K_RIGHT] or user_input[pygame.K_d]:
             self.move_rigth()
-        elif user_input[pygame.K_DOWN] or user_input[pygame.K_s]:
+        if user_input[pygame.K_DOWN] or user_input[pygame.K_s]:
             self.move_down()
-        elif user_input[pygame.K_UP] or user_input[pygame.K_w]:
+        if user_input[pygame.K_UP] or user_input[pygame.K_w]:
             self.move_up()
 
 
     def move_left(self):
-        if self.rect.left > 0:
-            self.rect.x -= 10
+         self.rect.x -= 10
+         if self.rect.right < 0:
+             self.rect.x = SCREEN_WIDTH-self.SPACESHIP_WIDTH
 
     def move_rigth(self):
-        if self.rect.right < SCREEN_WIDTH:
-            self.rect.x += 10
-            
+        self.rect.x += 10
+        if self.rect.left > SCREEN_WIDTH:
+            self.rect.x = 0
+
     def move_down(self):
         if self.rect.bottom < SCREEN_HEIGHT:
             self.rect.y += 10
@@ -41,3 +45,8 @@ class Spaceship(Sprite):
 
     def draw(self, screen):
         screen.blit(self.image,(self.rect.x, self.rect.y))
+        if self.rect.left < 0: 
+            screen.blit(self.image,(self.rect.x + SCREEN_WIDTH ,self.rect.y))
+        if self.rect.right > SCREEN_WIDTH:
+            screen.blit(self.image,(self.rect.x - SCREEN_WIDTH, self.rect.y))
+
